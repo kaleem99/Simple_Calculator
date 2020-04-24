7b8e7c6669703e253a154a485a199d7f35332184
kaleem <kaleem.mohammad@umuzi.org>
2020-04-24 14:39:19 +0200

    initial commit

---
+++ spec/simple_calculator_spec.js
@@ -0,0 +1,25 @@
+const {add, multiply} = require("../src/simple_calculator")
+
+describe("add", function(){
+    it("should add two numbers", function(){
+        expect(add(1, 2)).toEqual(3)
+    })
+    it("should add two numbers", function(){
+        expect(add(-1, -1)).toEqual(-2)
+    })
+    it("should add more than two numbers", function(){
+        expect(add(1, 2, 3, 4, 5)).toEqual(15)
+    })
+})
+
+describe("multiply", function(){
+    it("should multiply more than two numbers", function(){
+        expect(multiply(1, 2, 3, 4, 5)).toEqual(120)
+    })
+    it("should multiply two numbers", function(){
+        expect(multiply(1, 3)).toEqual(3)
+    })
+    it("should multiply two numbers", function(){
+        expect(multiply(-1, 3)).toEqual(-3)
+    })
+})
